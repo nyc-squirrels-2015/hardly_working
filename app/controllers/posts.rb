777f834cd8eba73860068f3fe37a478a649fbd4a
kjb085 & Jay Davis <pair+kjb085.planarian@devbootcamp.com>
2015-02-06 17:02:18 -0500

    Completed the post creation form

---
+++ app/controllers/posts.rb
@@ -0,0 +1,15 @@
+get '/posts/new' do
+  erb :'posts/create_post'
+end
+
+post '/posts' do
+  # session[:user_id] = 1
+  params[:new_post][:user_id] = current_user.id
+  Post.new(params[:new_post])
+  redirect '/'
+end
+
+get '/posts' do
+  @posts = Post.all
+  erb :'posts/all_posts', locals: {posts: Post.all}
+end
