1e4bb8b2e3a9bfbe8ad949527f0d603dea74821b
Jay <jhd@fastmail.net>
2015-02-07 18:08:29 -0500

    add class methods for incrementing/decrementing post id

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :tags
+
+  def self.next_post_id(current_id)
+    find_by(id: current_id + 1) ? current_id + 1 : 1
+  end
+
+  def self.previous_post_id(current_id)
+    find_by(id: current_id - 1) ? current_id - 1 : all.last.id
+  end
+
 end
