3b8b31998bbafbcc72ddccc5e873b0a00661818f
kjb085 & rkartzman <pair+kjb085.rkartzman@devbootcamp.com>
2015-02-07 21:16:24 -0500

    Really sorry again, but we got it working

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -1,22 +1,26 @@
 get '/' do
 
-  redirect '/game/post/1'
-  
+  redirect '/login'
+
 end
 
 get '/game/post/:id' do |id|
 
-  erb :'game/game', locals: { post: Post.find_by(id: id) }
+  @post = Post.find_by(id: id)
+  @comments = @post.comments.all
+
+  erb :'game/game'
 
 end
 
 post '/game/vote' do
 
   post = Post.find(params[:post_id])
-  
+
   if params[:vote] == 'vote up'
     post.positive_score += 1
-  else post.negative_score += 1
+  else
+    post.negative_score += 1
   end
 
   post.save
@@ -25,8 +29,8 @@ post '/game/vote' do
 end
 
 post '/game/rotate' do
-  
+
   current_post = Post.find(params[:post_id])
   redirect "/game/post/#{params[:direction] == 'back' ? Post.previous_post_id(current_post.id) : Post.next_post_id(current_post.id)}"
-  
+
 end
